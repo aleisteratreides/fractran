10cab28e24f5257fa1d48828c7e1ed24bcd629aa
Vsevolod Shchipunov <vsevolod.shchipunov@gmail.com>
2019-08-10 23:12:33 +0200

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for fractran
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,35 @@
+module Lib
+    ( someFunc,
+      calculateSequence,
+      calculateFinalNumber,
+    ) where
+
+
+import GHC.Real
+import Data.Maybe
+
+type Program = [Ratio Integer]
+
+nextNumber :: Integer -> Program -> Maybe Integer
+nextNumber _ [] = Nothing
+nextNumber n (x:xs) =
+  if denominator multiplied == 1 
+    then Just (numerator multiplied)
+    else nextNumber n xs
+    where multiplied = (x * fromIntegral n)
+
+calculateSequence :: Integer -> Program -> [Maybe Integer]
+calculateSequence n programm = 
+  if isNothing next
+    then []
+    else next : calculateSequence (fromJust next) programm
+    where next = nextNumber n programm
+
+calculateFinalNumber :: Integer -> Program -> Maybe Integer
+calculateFinalNumber n program = go (Just n) program Nothing
+  where 
+    go Nothing _ last = last
+    go n program _ = go (nextNumber (fromJust n) program) program n
+
+someFunc :: IO ()
+someFunc = putStrLn "Hello"
