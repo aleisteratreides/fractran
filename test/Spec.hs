be92b24726f509429a69489bdf5cf78ef8b3fd1e
Vsevolod Shchipunov <vsevolod.shchipunov@gmail.com>
2019-08-11 00:42:27 +0200

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,4 +8,6 @@ main :: IO ()
 main = hspec $ do
   describe "Lib.hs" $ do
     it "calculates sum of 1 and 1" $ do
-      calculateFinalNumber 6 [3 % 2] `shouldBe` (Just 9)
+      calculateFinalNumber (2^1*3^1) sumProgram `shouldBe` (Just $ 3^2)
+    it "substracts 62 from 65" $ do
+      calculateFinalNumber (2^65*3^62) subtractionProgram `shouldBe` (Just $ 2^3)
