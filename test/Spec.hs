b6a8d547b64e1398eeadc25b69d49c9c320eaba9
Vsevolod Shchipunov <vsevolod.shchipunov@gmail.com>
2019-08-10 23:37:23 +0200

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Lib
+import GHC.Real
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib.hs" $ do
+    it "calculates sum of 1 and 1" $ do
+      calculateFinalNumber 6 [3 % 2] `shouldBe` (Just 9)
