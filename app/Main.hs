fb1d4f3a65494eb522831df6ee38af97b0d3a318
Vsevolod Shchipunov <vsevolod@adjust.com>
2023-12-04 23:27:31 +0100

    prepare for the demo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
+import GHC.Real
+import Data.Maybe
 import Lib
 
+showSummation = calculateFinalNumber (2^num1 * 3^num2) sumProgram
+    where num1 = 100
+          num2 = 100
+
+showSubstraction = calculateFinalNumber (2^num1 * 3^num2) subtractionProgram
+    where num1 = 50
+          num2 = 47
+
+showSummationExplicit = calculateSequence (2^num1 * 3^num2) subtractionProgram
+    where num1 = 10
+          num2 = 7
+
+showPrimeNumbers = calculateSequence 10 primeNumbers
+
+isInt x = x == fromInteger (round x)
+
+logTenMaybe = logBase 10 . fromIntegral . fromJust
+
+isPowerOfTen Nothing = False
+isPowerOfTen x = isInt (logTenMaybe x)
+
 main :: IO ()
-main = print $ calculateFinalNumber (2^65*3^60) subtractionProgram
+main = do
+    -- print $ showSummation
+    -- print $ showSubstraction
+    -- print $ showSummationExplicit
+    let h = head showPrimeNumbers
+    let firstPrimes = take 10 (filter isPowerOfTen (showPrimeNumbers))
+    print $ zip firstPrimes (map logTenMaybe firstPrimes)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,4 @@
-module Lib
-    ( calculateSequence,
-      calculateFinalNumber,
-      sumProgram,
-      subtractionProgram,
-    ) where
-
+module Lib where
 
 import GHC.Real
 import Data.Maybe
@@ -32,9 +26,21 @@ calculateFinalNumber n program = go (Just n) program Nothing
     go Nothing _ last = last
     go n program _ = go (nextNumber (fromJust n) program) program n
 
-
 sumProgram :: Program
 sumProgram = [3 % 2]
 
 subtractionProgram :: Program
 subtractionProgram = [1 % 6]
+
+primeNumbers :: Program
+primeNumbers = [
+    7  % 3  ,
+    99 % 98 ,
+    13 % 49 ,
+    39 % 35 ,
+    36 % 91 ,
+    10 % 143,
+    49 % 13 ,
+    7  % 11 ,
+    1  % 2  ,
+    91 % 1]
